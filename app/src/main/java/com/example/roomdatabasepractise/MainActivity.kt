77fb2d3aeda53eb2943da1411dfc23cd0f7ab57f
Dipan-17 <93267025+Dipan-17@users.noreply.github.com>
2024-05-30 08:21:45 +0530

    Record adding function added

--- app/src/main/java/com/example/roomdatabasepractise/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabasepractise/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.roomdatabasepractise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import com.example.roomdatabasepractise.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding:ActivityMainBinding?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnAdd?.setOnClickListener {
+            //TODO call add record with employeeDAO
+        }
+
+    }
+
+    fun addRecord(employeeDAO: EmployeeDAO){
+        val name=binding?.etName?.text.toString()
+        val email=binding?.etEmailId?.text.toString()
+        if(name.isNotEmpty() && email.isNotEmpty()){
+            //coroutine
+            lifecycleScope.launch {
+                employeeDAO.insert(EmployeeEntity(name=name,email=email))
+
+                Toast.makeText(applicationContext,"Record saved",Toast.LENGTH_SHORT).show()
+                binding?.etName?.text?.clear()
+                binding?.etEmailId?.text?.clear()
+            }
+        }else{
+            Toast.makeText(applicationContext,"Please fill records first",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        binding=null
+        super.onDestroy()
     }
 }
